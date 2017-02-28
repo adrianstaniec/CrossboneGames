f3eba060ef1c7a6419ac3435c8ba2d4ef4346f94
Adrian Staniec <adrianstaniec@gmail.com>
2017-02-28 11:10:47 +0100

    TicTacToe: Add --verbose switch to the match

--- TicTacToe/match.py
+++ TicTacToe/match.py
@@ -6,33 +6,37 @@ from backend import model
 from backend import ai
 
 
-def play_once(agents, state):
+def play_once(agents, state, verbose=False):
     trace = []
     while True:
         state.matrix, state.player = agents[state.player].mark_spot(state.matrix)
+        if verbose:
+            print(state.matrix, end='\n\n')
         trace.append(state.matrix)
         winner = model.check_winner(state.matrix)
         if winner != model.NOONE:
             break
     return winner, trace
 
-def play_multiple(x_level, o_level , num_matches):
-    agents = {model.PLAYER_X : ai.Factory(model.PLAYER_X, x_level),
-              model.PLAYER_O : ai.Factory(model.PLAYER_O, o_level)}
 
-    stats = {model.PLAYER_X : 0,
-             model.PLAYER_O : 0,
-             model.DRAW : 0}
+def play_multiple(x_level, o_level, num_matches, verbose=False):
+    agents = {model.PLAYER_X: ai.Factory(model.PLAYER_X, x_level),
+              model.PLAYER_O: ai.Factory(model.PLAYER_O, o_level)}
+
+    stats = {model.PLAYER_X: 0,
+             model.PLAYER_O: 0,
+             model.DRAW: 0}
 
     for i in range(num_matches):
         if i < num_matches / 2:
             state = model.State(model.PLAYER_X)
         else:
             state = model.State(model.PLAYER_O)
-        winner, _ = play_once(agents, state)
+        winner, _ = play_once(agents, state, verbose)
         stats[winner] += 1
 
     return stats
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -43,9 +47,11 @@ if __name__ == "__main__":
                         help="O player level [0-{}]".format(ai.NUM-1))
     parser.add_argument("-n", type=int, default=10,
                         help="number of matches")
-    args =  parser.parse_args()
+    parser.add_argument("-v", "--verbose", action="store_true",
+                        help="print board after every move")
+    args = parser.parse_args()
 
-    stats = play_multiple(args.x, args.o , args.n)
+    stats = play_multiple(args.x, args.o, args.n, args.verbose)
 
     print(" X (L{}) | O (L{}) |   =    ".format(args.x, args.o))
     print("{: ^8}|{: ^8}|{: ^8}".format(stats[model.PLAYER_X],
